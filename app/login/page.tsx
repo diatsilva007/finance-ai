732dd577e3c2313ede70eb569569539bd343b085
diatsilva007 <diatsilva007@gmail.com>
2024-11-06 23:47:06 -0300

    chore: add git hooks setup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,47 +6,48 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 const LoginPage = async () => {
-    const { userId } = await auth();
-    if (userId) {  
-        redirect("/");
-    }
-    return (
-        <div className="grid grid-cols-2">
-            {/*ESQUERDA*/}
-            <div className="flex flex-col h-full p-8 max-w-[550px] mx-auto">
-                <Image src="/logo.svg"
-                    width={173}
-                    height={39}
-                    alt="Finance AI"
-                    className="mb-8"
-                />
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
+  return (
+    <div className="grid h-full grid-cols-2">
+      {/*ESQUERDA*/}
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/logo.svg"
+          width={173}
+          height={39}
+          alt="Finance AI"
+          className="mb-8"
+        />
 
-                <h1 className="text-4xl font-bold mb-3">Bem vindo</h1>
-                <p className="text-muted-foreground mb-8">
-                    A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.
-                </p>
+        <h1 className="mb-3 text-4xl font-bold">Bem vindo</h1>
+        <p className="mb-8 text-muted-foreground">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
 
-                <SignInButton>
-                    <Button variant="outline">
-                        <LogInIcon className="mr-2" />
-                        Fazer login ou criar conta
-                    </Button>
-                </SignInButton>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
+      </div>
 
-            </div>
-
-            {/*DIREITA*/}
-            <div className="relative h-full w-full">
+      {/*DIREITA*/}
+      {/*<div className="relative h-full w-full">
                 <Image
                     src="/login.png"
                     alt="Faça login"
                     fill
-                    className="object-c"
+                    className="object-cover"
                 />
-            </div>
+            <div>*/}
+    </div>
+  );
+};
 
-        </div>
-    );
-}
-
-export default LoginPage;   
+export default LoginPage;
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,15 @@ import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
 const Home = async () => {
   const { userId } = await auth();
   if (!userId) {
-    redirect("/login")
+    redirect("/login");
   }
 
   return (
-    <div className="h-full flex items-center justify-center">
-      <UserButton showName/>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
     </div>
   );
 };
